7a7a20d09340393808d15726b7ec69b89d15fdf0
ComputerMaster1st <mr_m.a_thompson@hotmail.co.uk>
2017-08-25 22:04:49 +0100

    Added CreateNote

--- NotesManager.cs
+++ NotesManager.cs
@@ -1,15 +1,21 @@
+using System.Threading.Tasks;
 using MongoDB.Driver;
 
 namespace notetoself_mongodb
 {
     public class NotesManager {
         MongoClient Mongo = new MongoClient("mongodb://notetoself:cmaster1st@localhost:27017");
-        IMongoDatabase Data;
+        IMongoCollection<Note> Collection;
         
         public NotesManager() {
-            Data = Mongo.GetDatabase("notetoself");
+            IMongoDatabase Data = Mongo.GetDatabase("notetoself");
+            Collection = Data.GetCollection<Note>("notes");
         }
 
-        
+        public async Task<Note> CreateNote(ulong UserId) {
+            Note Note = new Note(UserId);
+            await Collection.InsertOneAsync(Note);
+            return Note;
+        }
     }
 }
